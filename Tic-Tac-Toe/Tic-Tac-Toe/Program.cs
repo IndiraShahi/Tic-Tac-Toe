0f2e6d2f68c64713486623e3918b119a11d865dc
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-05 01:41:04 +0530

    [Indira] ADDED: Created a board for the game

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -2,11 +2,22 @@
 
 namespace Tic_Tac_Toe
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's play Tic-Tac-Toe!");
+           // Console.WriteLine("Let's play Tic-Tac-Toe!");
+            char[] board = createBoard();
+        }
+        private static char[]  createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+
         }
     }
 }
