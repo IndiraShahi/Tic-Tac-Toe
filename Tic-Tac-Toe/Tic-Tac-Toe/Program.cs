a18edb838d7d80ad28282cc5a77f45d7c30a2a17
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-05 17:06:29 +0530

    [Indira] ADDED: Check for free space before making a move

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -6,11 +6,12 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Let's play Tic-Tac-Toe!");
+            // Console.WriteLine("Let's play Tic-Tac-Toe!");
             char[] board = createBoard();
             showBoard(board);
             char userLetter = chooseUserLetter();
             int userMove = getUserMove(board);
+            makeMove(board, userMove, userLetter);
 
         }
         private static char[] createBoard()
@@ -36,6 +37,25 @@ namespace Tic_Tac_Toe
             Console.WriteLine("-----------");
             Console.WriteLine("\n" + board[7] + " | " + board[8] + " | " + board[9]);
         }
-
+        private static int getUserMove(char[] board)
+        {
+            int[] validCells = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            while (true)
+            {
+                Console.WriteLine("What is your next move? (1-9):");
+                int index = Convert.ToInt32(Console.ReadLine());
+                if (Array.Find<int>(validCells, element => element == index) != 0 && isSpaceFree(board, index))
+                    return index;
+            }
+        }
+        private static bool isSpaceFree(char[] board, int index)
+        {
+            return board[index] == ' ';
+        }
+        private static void makeMove(char[] board, int index, char letter)
+        { 
+            Boolean spaceFree = isSpaceFree(board, index);
+            if (spaceFree) board[index] = letter;
+        }
     }
 }
